82b8b253159f02ffd6c540a7cfc8a5b0d265302b
Lanchon <lanchon@gmail.com>
2024-09-25 00:11:35 -0300

    Remove unnecessary bootargs

--- configure-uboot.sh
+++ configure-uboot.sh
@@ -173,7 +173,7 @@ fw_setenv boot_write_recovery_from_tftp 'run boot_set_type_initramfs; run boot_s
 fw_setenv boot_set_ip 'setenv ipaddr 192.168.1.1; setenv netmask 255.255.255.0; setenv serverip 192.168.1.2'
 
 fw_setenv boot_set_type_initramfs 'setenv loadaddr 44000000; setenv bootargs console=ttyMSM0,115200n8'
-fw_setenv boot_set_type_squashfs 'setenv loadaddr 44000000; setenv bootargs console=ttyMSM0,115200n8 mmc_mid=0x15 boot_signedimg mmc_mid=0x15 boot_signedimg mmc_mid=0x15 boot_signedimg root=$ROOTFS rootwait'
+fw_setenv boot_set_type_squashfs 'setenv loadaddr 44000000; setenv bootargs console=ttyMSM0,115200n8 root=$ROOTFS rootwait'
 
 
 ## U-Boot hack (WARNING: depends on U-Boot version!)
